bfc0142e8780d02e955128bc334c58c534057152
Karim <benkiranemedkarim@gmail.com>
2024-05-18 18:07:57 +0100

    Amélioration du code

--- src/main/java/com/example/giefrontend1/Controllers/Commercant/UpdateContactController.java
+++ src/main/java/com/example/giefrontend1/Controllers/Commercant/UpdateContactController.java
@@ -14,6 +14,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 
 public class UpdateContactController implements Initializable {
     @FXML
@@ -171,6 +172,8 @@ public class UpdateContactController implements Initializable {
                     contactDTOObservableList.setAll(SearchResultController.getUpdatedContactList());
                 }
                 showAlert(AlertType.INFORMATION, "Succès", "Contact modifié avec succès!");
+                Stage stage = (Stage)typeContactChoiceBox.getScene().getWindow();
+                stage.close();
             }
             else{
                 showAlert(AlertType.ERROR, "Erreur", "Erreur lors de la modification du contact");
@@ -186,6 +189,8 @@ public class UpdateContactController implements Initializable {
                     contactDTOObservableList.setAll(SearchResultController.getUpdatedContactList());
                 }
                 showAlert(AlertType.INFORMATION, "Succès", "Contact modifié avec succès!");
+                Stage stage = (Stage)typeContactChoiceBox.getScene().getWindow();
+                stage.close();
             }
             else{
                 showAlert(AlertType.ERROR, "Erreur", "Erreur lors de la modification du contact");
